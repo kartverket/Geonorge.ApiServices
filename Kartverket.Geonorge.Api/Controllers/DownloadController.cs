a7ccd2a7e3109421472a08908526628e9ba1142e
Dag Olav <dagolav@arkitektum.no>
2016-09-09 16:43:10 +0200

    Download check

---
+++ Kartverket.Geonorge.Api/Controllers/DownloadController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Kartverket.Geonorge.Api.Services;
+
+namespace Kartverket.Geonorge.Api.Controllers
+{
+    public class DownloadController : ApiController
+    {
+        /// <summary>
+        /// Catalogue in dcat format
+        /// </summary>
+        [System.Web.Http.Route("metadata/downloadinvalid")]
+        [System.Web.Http.HttpGet]
+        public List<MetadataEntry> GetDownloads()
+        {
+            return new DownloadService().CheckDownload();
+        }
+    }
+}
