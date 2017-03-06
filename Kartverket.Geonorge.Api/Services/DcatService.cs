fb60d81dc44f38df8eda23c04d9f4026ced83878
Dag Olav <dagolav@arkitektum.no>
2017-03-06 13:22:23 +0100

    Display more services OGC:WFS and OGC:WCS

--- Kartverket.Geonorge.Api/Services/DcatService.cs
+++ Kartverket.Geonorge.Api/Services/DcatService.cs
@@ -107,7 +107,7 @@ namespace Kartverket.Geonorge.Api.Services
 
             Dictionary<string, XmlNode> foafAgents = new Dictionary<string, XmlNode>();
             Dictionary<string, XmlNode> vcardKinds = new Dictionary<string, XmlNode>();
-            Dictionary<string, XmlNode> wmsServices = new Dictionary<string, XmlNode>();
+            Dictionary<string, XmlNode> services = new Dictionary<string, XmlNode>();
 
             for (int d = 0; d < metadataSets.Items.Length; d++)
             {
@@ -345,7 +345,7 @@ namespace Kartverket.Geonorge.Api.Services
 
                     }
 
-                    // Dataset distribution wms
+                    // Dataset distributions
                     // Get distribution from index in kartkatalog 
                     string metadataUrl = System.Web.Configuration.WebConfigurationManager.AppSettings["KartkatalogenUrl"] + "api/getdata/" + uuid;
                     System.Net.WebClient c = new System.Net.WebClient();
@@ -355,52 +355,72 @@ namespace Kartverket.Geonorge.Api.Services
 
                     dynamic metadata = Newtonsoft.Json.Linq.JObject.Parse(json);
 
-                    if (metadata != null && metadata.ServiceDistributionUrlForDataset != null 
-                        && metadata.ServiceDistributionProtocolForDataset == "OGC:WMS")
+                    if (metadata != null && metadata.Related != null)
                     {
-                        //Map distribution to dataset
-                        XmlElement distributionDataset = doc.CreateElement("dcat", "distribution", xmlnsDcat);
-                        distributionDataset.SetAttribute("resource", xmlnsRdf, kartkatalogenUrl + "Metadata/uuid/" + metadata.ServiceUuid.Value);
-                        dataset.AppendChild(distributionDataset);
+                        foreach (var related in metadata.Related)
+                        {
+                            var uuidService = related.Uuid.Value;
 
-                        XmlElement distribution = doc.CreateElement("dcat", "Distribution", xmlnsDcat);
-                        distribution.SetAttribute("about", xmlnsRdf, kartkatalogenUrl + "Metadata/uuid/" +  metadata.ServiceUuid.Value);
+                            if(related.DistributionDetails != null)
+                            {
 
-                        XmlElement distributionTitle = doc.CreateElement("dct", "title", xmlnsDct);
-                        distributionTitle.SetAttribute("xml:lang", "no");
-                        distributionTitle.InnerText = "OGC:WMS";
-                        distribution.AppendChild(distributionTitle);
+                                var protocol = related.DistributionDetails.Protocol.Value;
+                                var serviceDistributionUrl = related.DistributionDetails.URL.Value;
 
-                        distributionTitle = doc.CreateElement("dct", "title", xmlnsDct);
-                        distributionTitle.SetAttribute("xml:lang", "en");
-                        distributionTitle.InnerText = "OGC:WMS";
-                        distribution.AppendChild(distributionTitle);
+                                string protocolName = protocol;
+                                if (protocolName.Contains(":"))
+                                    protocolName = protocolName.Split(':')[1];
 
-                        XmlElement distributionDescription = doc.CreateElement("dct", "description", xmlnsDct);
-                        distributionDescription.SetAttribute("xml:lang", "no");
-                        distributionDescription.InnerText = "Visningstjeneste (WMS)";
-                        distribution.AppendChild(distributionDescription);
+                                if(protocol == "OGC:WMS" || protocol == "OGC:WFS" || protocol == "OGC:WCS")
+                                {
 
-                        distributionDescription = doc.CreateElement("dct", "description", xmlnsDct);
-                        distributionDescription.SetAttribute("xml:lang", "en");
-                        distributionDescription.InnerText = "View Service (WMS)";
-                        distribution.AppendChild(distributionDescription);
+                                    XmlElement distributionDataset = doc.CreateElement("dcat", "distribution", xmlnsDcat);
+                                    distributionDataset.SetAttribute("resource", xmlnsRdf, kartkatalogenUrl + "Metadata/uuid/" + uuidService);
+                                    dataset.AppendChild(distributionDataset);
 
-                        XmlElement distributionFormat = doc.CreateElement("dct", "format", xmlnsDct);
-                        distributionFormat.InnerText = "WMS";
-                        distribution.AppendChild(distributionFormat);
+                                    XmlElement distribution = doc.CreateElement("dcat", "Distribution", xmlnsDcat);
+                                    distribution.SetAttribute("about", xmlnsRdf, kartkatalogenUrl + "Metadata/uuid/" + uuidService);
 
-                        XmlElement distributionAccessURL = doc.CreateElement("dcat", "accessURL", xmlnsDcat);
-                        distributionAccessURL.SetAttribute("resource", xmlnsRdf, metadata.ServiceDistributionUrlForDataset.Value);
-                        distribution.AppendChild(distributionAccessURL);
+                                    XmlElement distributionTitle = doc.CreateElement("dct", "title", xmlnsDct);
+                                    distributionTitle.SetAttribute("xml:lang", "no");
+                                    distributionTitle.InnerText = protocol;
+                                    distribution.AppendChild(distributionTitle);
 
-                        XmlElement distributionLicense = doc.CreateElement("dct", "license", xmlnsDct);
-                        if (data.Constraints != null && !string.IsNullOrEmpty(data.Constraints.OtherConstraintsLink))
-                            distributionLicense.SetAttribute("resource", xmlnsRdf, data.Constraints.OtherConstraintsLink);
-                        distribution.AppendChild(distributionLicense);
+                                    distributionTitle = doc.CreateElement("dct", "title", xmlnsDct);
+                                    distributionTitle.SetAttribute("xml:lang", "en");
+                                    distributionTitle.InnerText = protocol;
+                                    distribution.AppendChild(distributionTitle);
 
-                        if (!wmsServices.ContainsKey(metadata.ServiceDistributionUrlForDataset.Value))
-                            wmsServices.Add(metadata.ServiceDistributionUrlForDataset.Value, distribution);
+                                    XmlElement distributionDescription = doc.CreateElement("dct", "description", xmlnsDct);
+                                    distributionDescription.SetAttribute("xml:lang", "no");
+                                    distributionDescription.InnerText = "Visningstjeneste ("+ protocolName + ")";
+                                    distribution.AppendChild(distributionDescription);
+
+                                    distributionDescription = doc.CreateElement("dct", "description", xmlnsDct);
+                                    distributionDescription.SetAttribute("xml:lang", "en");
+                                    distributionDescription.InnerText = "View Service ("+ protocolName + ")";
+                                    distribution.AppendChild(distributionDescription);
+
+                                    XmlElement distributionFormat = doc.CreateElement("dct", "format", xmlnsDct);
+                                    distributionFormat.InnerText = protocolName;
+                                    distribution.AppendChild(distributionFormat);
+
+                                    XmlElement distributionAccessURL = doc.CreateElement("dcat", "accessURL", xmlnsDcat);
+                                    distributionAccessURL.SetAttribute("resource", xmlnsRdf, serviceDistributionUrl);
+                                    distribution.AppendChild(distributionAccessURL);
+
+                                    XmlElement distributionLicense = doc.CreateElement("dct", "license", xmlnsDct);
+                                    if (data.Constraints != null && !string.IsNullOrEmpty(data.Constraints.OtherConstraintsLink))
+                                        distributionLicense.SetAttribute("resource", xmlnsRdf, data.Constraints.OtherConstraintsLink);
+                                    distribution.AppendChild(distributionLicense);
+
+                                    if (!services.ContainsKey(serviceDistributionUrl))
+                                        services.Add(serviceDistributionUrl, distribution);
+
+
+                                }
+                            }
+                        }
 
                     }
 
@@ -446,9 +466,9 @@ namespace Kartverket.Geonorge.Api.Services
                 root.AppendChild(vcardKind.Value);
             }
 
-            foreach (var wmsService in wmsServices)
+            foreach (var service in services)
             {
-                root.AppendChild(wmsService.Value);
+                root.AppendChild(service.Value);
             }
 
             foreach (var conceptObject in ConceptObjects)
