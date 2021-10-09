0cd31f56901ba59bba095612fe4cdf4053583303
Dag Olav Dahle <dagolav@arkitektum.no>
2021-10-09 10:40:25 +0200

    Update atom feed geonetwork

--- Kartverket.Geonorge.Api/Services/FeedService.cs
+++ Kartverket.Geonorge.Api/Services/FeedService.cs
@@ -35,23 +35,89 @@ namespace Kartverket.Geonorge.Api.Services
                    Uuid = g.Key,
                    Datasets = g.Select(dataset => new {
                        dataset.Url,
-                       dataset.Organization
+                       dataset.Organization,
+                       dataset.LastUpdated
                    })
                }).ToList();
 
             foreach(var dataset in datasets)
             {
-                var uuid = dataset.Uuid;
+                try
+                { 
+                var metadataInfo = new UpdateMetadataInformation();
+                metadataInfo.Uuid = dataset.Uuid;
+                metadataInfo.Distributions = new List<SimpleDistribution>();
                 foreach (var item in dataset.Datasets)
                 {
-                    var url = item.Url;
-                    var organization = item.Organization;
+                    //metadataInfo.DatasetDateUpdated = Convert.ToDateTime(item.LastUpdated);
 
-                    //Distribusjonstype=W3C:AtomFeed
-                    //Format: gml 3.2.1?
+                    SimpleDistribution simpleDistribution = new SimpleDistribution();
+                    simpleDistribution.Organization = item.Organization;
+                    simpleDistribution.Protocol = "W3C:AtomFeed";
+                    simpleDistribution.URL = item.Url;
+
+                    metadataInfo.Distributions.Add(simpleDistribution);
                 }
 
-                //todo save to geonetwork
+                System.Collections.Specialized.NameValueCollection settings = System.Web.Configuration.WebConfigurationManager.AppSettings;
+                string server = settings["GeoNetworkUrl"];
+                string usernameGeonetwork = settings["GeoNetworkUsername"];
+                string password = settings["GeoNetworkPassword"];
+                string geonorgeUsername = settings["GeonorgeUsername"];
+
+
+                GeoNorge api = new GeoNorge(usernameGeonetwork, password, server);
+                api.OnLogEventDebug += new GeoNorgeAPI.LogEventHandlerDebug(LogEventsDebug);
+                api.OnLogEventError += new GeoNorgeAPI.LogEventHandlerError(LogEventsError);
+
+                var metadata = api.GetRecordByUuid(metadataInfo.Uuid);
+
+                SimpleMetadata simpleMetadata = new SimpleMetadata(metadata);
+
+                List<SimpleDistribution> distributionFormats = simpleMetadata.DistributionsFormats;
+
+                List<SimpleDistribution> distributionFormatsUpdated = new List<SimpleDistribution>();
+
+                if (HasAtomFeed(metadataInfo.Distributions))
+                {
+
+                    foreach (var distribution in distributionFormats)
+                    {
+                        if (distribution.Protocol != "W3C:AtomFeed")
+                        {
+                            distributionFormatsUpdated.Add(distribution);
+                        }
+                    }
+
+                }
+                else
+                {
+                    distributionFormatsUpdated = distributionFormats;
+                }
+
+                distributionFormatsUpdated.InsertRange(0, metadataInfo.Distributions);
+
+                simpleMetadata.DistributionsFormats = distributionFormatsUpdated;
+                simpleMetadata.DistributionDetails = new SimpleDistributionDetails
+                {
+                    URL = distributionFormatsUpdated[0].URL,
+                    Protocol = distributionFormatsUpdated[0].Protocol,
+                    UnitsOfDistribution = distributionFormatsUpdated[0].UnitsOfDistribution
+                };
+
+
+                simpleMetadata.DateMetadataUpdated = DateTime.Now;
+
+                if (metadataInfo.DatasetDateUpdated.HasValue)
+                    simpleMetadata.DateUpdated = metadataInfo.DatasetDateUpdated;
+
+                api.MetadataUpdate(simpleMetadata.GetMetadata(), CreateAdditionalHeadersWithUsername(geonorgeUsername, "true"));
+                Log.Info($"Metadata updated for uuid: {metadataInfo.Uuid}");
+                }
+                catch(Exception ex)
+                {
+                    Log.Error("Error updating metadata" + ex);
+                }
             }
 
             return Task.CompletedTask;
@@ -70,6 +136,7 @@ namespace Kartverket.Geonorge.Api.Services
 
             return geonorgeDatasets.Concat(nguDatasets).Concat(nibioDatasets).Concat(miljodirektoratetDatasets).ToList();
         }
+
         public List<Dataset> GetDatasetsFromUrl(string url)
         {
             try
@@ -85,6 +152,48 @@ namespace Kartverket.Geonorge.Api.Services
 
             return new List<Dataset>();
         }
+
+        private bool HasAtomFeed(List<SimpleDistribution> distributions)
+        {
+            for (int i = 0; i < distributions.Count; i++)
+            {
+                if (distributions[i].Protocol == "W3C:AtomFeed")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void LogEventsDebug(string log)
+        {
+
+            Log.Debug(log);
+        }
+
+        private void LogEventsError(string log, Exception ex)
+        {
+            Log.Error(log, ex);
+        }
+
+        public Dictionary<string, string> CreateAdditionalHeadersWithUsername(string username, string published = "")
+        {
+            Dictionary<string, string> header = new Dictionary<string, string> { { "GeonorgeUsername", username } };
+
+            header.Add("GeonorgeOrganization", "Kartverket");
+            header.Add("GeonorgeRole", "nd.metadata_admin");
+            header.Add("published", published);
+
+            return header;
+        }
+    }
+
+    public class UpdateMetadataInformation
+    {
+        public string Uuid { get; set; }
+        public List<SimpleDistribution> Distributions { get; set; }
+        public DateTime? DatasetDateUpdated { get; set; }
+
     }
 
     public class Dataset
