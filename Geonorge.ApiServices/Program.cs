3adb8d7d6f9dea29a805e8e5e4d0e22ba9d52d12
Dag Olav Dahle <dagolav@arkitektum.no>
2025-09-17 08:56:56 +0200

    Use swagger

--- Geonorge.ApiServices/Program.cs
+++ Geonorge.ApiServices/Program.cs
@@ -58,12 +58,8 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+ app.UseSwagger();
+ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
